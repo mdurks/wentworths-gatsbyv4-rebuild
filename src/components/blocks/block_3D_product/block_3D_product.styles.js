8d8f773b21dca84acc863545c6f367abe7821d46
mdurks <mdurks@gmail.com>
2022-09-20 20:39:09 +0100

    Removed everything except homepage and 404 pretty much.

--- src/components/blocks/block_3D_product/block_3D_product.styles.js
+++
@@ -1,4 +0,0 @@
-import styled from "styled-components"
-import css_breakpoints from "../../../common/css_breakpoints"
-
-export const Canvas_3D_product = styled.canvas``
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,14 @@
 import React from "react"
-
-// import Layout from "../components/layout"
-import SEO from "../components/layout/seo"
-import { Styled_SiteContainer } from "../styles/commonStyles"
+// import { Styled_SiteContainer } from "../styles/commonStyles"
 
 const NotFoundPage = () => (
   <>
-    <Styled_SiteContainer>
-      <SEO title="404: Not found" />
-      <h1>We're sorry, the page wasn't found</h1>
-      <p>
-        This page doesn't exist or it's being built and hasn't been hooked in
-        yet.
-      </p>
-    </Styled_SiteContainer>
+    {/* <Styled_SiteContainer> */}
+    <h1>We're sorry, the page wasn't found</h1>
+    <p>
+      This page doesn't exist or it's being built and hasn't been hooked in yet.
+    </p>
+    {/* </Styled_SiteContainer> */}
   </>
 )
 
